565d8ff9067443a19734f05eb9b37b46a52d2871
Peter Caprioli <petercap@kth.se>
2014-09-11 20:06:20 +0200

    pass3

--- indexer.cpp
+++ indexer.cpp
@@ -1,15 +1,38 @@
 #include <iostream>
 #include "latmanshash.hpp"
+#include <stdio.h>
+#include <unordered_map>
+#include <vector>
+
 using namespace std;
-int main(void)
-{		
-	// while(true){
-	// 	char word[45];
-	// 	int pointer;
-	// 	cin.getline(word, 45, ' ');
-	// 	cin >> pointer;
-	// 	cin.get();
-		
-	// }
-    return 0;
+typedef unordered_map<string,vector<uint32_t>> pindex; 
+
+FILE * openIndex(string name) {
+	FILE * file;
+	file = fopen(name.c_str(), "w");
+	if (file == NULL) {
+		cerr << "Unable to open index for writing\n";
+		exit (1);
+	}
+	return file;
+}
+
+int main(void) {
+	pindex pindex;
+	FILE * file = openIndex("index");
+	//fseek(file, 10000, SEEK_SET);
+	
+
+	while(cin){
+		char word[45];
+		uint32_t pointer;
+		cin.getline(word, 45, ' ');
+		cin >> pointer;
+		cin.get();
+		pindex[word].push_back(pointer);
+	}
+
+	cout << "Size: " << pindex.size() << "\n";
+
+	return 0;
 }
